f6485ea76962f6654c2e2c1bfb1613bc1fb49aa0
yuvaraj <yuvaraj.codes@gmail.com>
2026-02-07 11:28:53 +0530

    update club card

--- src/pages/Clubs/ClubCard.js
+++ src/pages/Clubs/ClubCard.js
@@ -6,18 +6,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function ClubCard({ name, url, imgUrl, tagline }) {
     return (
-            <div className="card_div box col col-6">
+            <div className="club_card_container box col col-6">
             <span></span>
             <span></span>
             <span></span>
             <span></span>
-                <Link to={url}>
-                    <img className="card_image" src={imgUrl} alt="club" />
-                    <div className="card_content">
-                        <div className="card_title">{name}</div>
-                        <div className="card_desc">{tagline}</div>
-                    </div>
-                </Link>
+                <div className="club_card_image_wrapper">
+                    <img className="club_card_image" src={imgUrl} alt="club" />
+                </div>
+                <div className="club_card_content">
+                    <h3 className="club_card_title">{name}</h3>
+                    <Link to={url} className="club_view_button">
+                        View Events
+                    </Link>
+                </div>
             </div> 
     );
 }
